53aa08648215dfd51d7bbb081a67961a4b12b62f
yangtaeyoung93 <ngj100@naver.com>
2025-04-17 06:09:46 +0900

    [feat] Add productviewcontroller

---
+++ src/main/java/com/commerce/content/controller/product/ProductViewController.java
@@ -0,0 +1,18 @@
+package com.commerce.content.controller.product;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class ProductViewController {
+
+    @GetMapping("/products")
+    public String getProducts(){
+
+        return "/product/products";
+    }
+}
